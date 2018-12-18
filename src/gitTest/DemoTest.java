dd95903ee56d9ab9298740ff655a9d35eaf849a3
satish396 <rongalasatish52@gmail.com>
2018-12-18 15:34:25 +0530

    Adding Commit

---
+++ src/gitTest/DemoTest.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoTest {
+
+	@Test
+	public void Login(){
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.faceboo.com");
+		driver.findElement(By.id("email"));
+	}
+}
