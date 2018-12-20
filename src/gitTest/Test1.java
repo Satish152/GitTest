249c64aea32e244cbd74e04aaa6922f392a22f48
satish396 <rongalasatish52@gmail.com>
2018-12-20 16:29:45 +0530

    New Test Class Deployed

---
+++ src/gitTest/Test1.java
@@ -0,0 +1,16 @@
+package gitTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	@Test
+	public static void Chrome(){
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+	}
+}
